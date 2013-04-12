e1298ff221f37c3a638cd050f3aa02f1be3eaafc
Christoper Herman <crherma2@ncsu.edu>
2013-04-11 21:34:38 -0400

    added loss matrix and matrix dimensions

--- src/main.F90
+++ src/main.F90
@@ -3,6 +3,7 @@ program main
 !-references
   
   use initialize, only: initialize_run
+  use loss,       only: create_lossmatrix
   use matrix,     only: test_matrix
 
 !-program options
@@ -12,7 +13,11 @@ program main
 !-begin execution
 
   call test_matrix()
+  
   ! initialize input
   call initialize_run()  
+
+  ! create loss matrix
+  call create_lossmatrix()
 
 end program main
